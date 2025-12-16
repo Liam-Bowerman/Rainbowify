2d26652212f27fbd2b88d1e98b1e3158ed2365c7
Liam-Bowerman <liboad0@student.oaisd.org>
2025-12-16 18:59:48 +0000

    Finished rainbow function!

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using Rainbows;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Rainbowify.Write("Hello my name is emmanuel");
+    }
+}
---
+++ Rainbowify.cs
@@ -0,0 +1,52 @@
+namespace Rainbows
+{
+    public class Rainbowify
+    {
+        public static string Write(string userTextToMakeRainbow)
+        {
+            int i = 1;
+            foreach (char c in userTextToMakeRainbow)
+            {
+                string foreColor = "\x1b[38;2" + ";" + 255 + ";" + 0 + ";" + 0 + "m";
+                switch (i)
+                {
+                    case 1:
+                        foreColor = "\x1b[38;2" + ";" + 255 + ";" + 0 + ";" + 0 + "m";
+                        Console.Write(foreColor + c);
+                        break;
+                    case 2:
+                        foreColor = "\x1b[38;2" + ";" + 209 + ";" + 111 + ";" + 13 + "m";
+                        Console.Write(foreColor + c);
+                        break;
+                    case 3:
+                        foreColor = "\x1b[38;2" + ";" + 255 + ";" + 255 + ";" + 0 + "m";
+                        Console.Write(foreColor + c);
+                        break;
+                    case 4:
+                        foreColor = "\x1b[38;2" + ";" + 0 + ";" + 255 + ";" + 0 + "m";
+                        Console.Write(foreColor + c);
+                        break;
+                    case 5:
+                        foreColor = "\x1b[38;2" + ";" + 0 + ";" + 0 + ";" + 255 + "m";
+                        Console.Write(foreColor + c);
+                        break;
+                    case 6:
+                        foreColor = "\x1b[38;2" + ";" + 128 + ";" + 0 + ";" + 128 + "m";
+                        Console.Write(foreColor + c);
+                        break;
+                    default:
+                        break;
+                }
+                i++;
+                if (i > 6)
+                {
+                    i = 1;
+                }
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            Console.WriteLine("");
+            return "";
+            
+        }
+    }
+}
