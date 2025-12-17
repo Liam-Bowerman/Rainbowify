0238ebb8987c810e54e19b5d98654b5a68a38a8c
Liam-Bowerman <liboad0@student.oaisd.org>
2025-12-17 08:10:33 -0500

    Enhance rainbow text output with additional colors

--- Rainbowify.cs
+++ Rainbowify.cs
@@ -15,6 +15,50 @@ namespace Rainbows
                         Console.Write(foreColor + c);
                         break;
                     case 2:
+                        foreColor = "\x1b[38;2" + ";" + 242 + ";" + 136 + ";" + 29 + "m";
+                        Console.Write(foreColor + c);
+                        break;
+                    case 3:
+                        foreColor = "\x1b[38;2" + ";" + 255 + ";" + 255 + ";" + 0 + "m";
+                        Console.Write(foreColor + c);
+                        break;
+                    case 4:
+                        foreColor = "\x1b[38;2" + ";" + 0 + ";" + 255 + ";" + 0 + "m";
+                        Console.Write(foreColor + c);
+                        break;
+                    case 5:
+                        foreColor = "\x1b[38;2" + ";" + 0 + ";" + 0 + ";" + 255 + "m";
+                        Console.Write(foreColor + c);
+                        break;
+                    case 6:
+                        foreColor = "\x1b[38;2" + ";" + 128 + ";" + 0 + ";" + 128 + "m";
+                        Console.Write(foreColor + c);
+                        break;
+                    default:
+                        break;
+                }
+                i++;
+                if (i > 6)
+                {
+                    i = 1;
+                }
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            return ""; 
+        }
+        public static string WriteLine(string userTextToMakeRainbow)
+        {
+            int i = 1;
+            foreach (char c in userTextToMakeRainbow)
+            {
+                string foreColor = "\x1b[38;2" + ";" + 255 + ";" + 0 + ";" + 0 + "m";
+                switch (i)
+                {
+                    case 1:
+                        foreColor = "\x1b[38;2" + ";" + 255 + ";" + 0 + ";" + 0 + "m";
+                        Console.Write(foreColor + c);
+                        break;
+                    case 2:
                         foreColor = "\x1b[38;2" + ";" + 209 + ";" + 111 + ";" + 13 + "m";
                         Console.Write(foreColor + c);
                         break;
@@ -46,7 +90,28 @@ namespace Rainbows
             }
             Console.WriteLine("");
             return "";
-            
+
+        }
+        public static string WriteColor(string userTextToMakeRainbow, int red, int green, int blue)
+        {
+            foreach (char c in userTextToMakeRainbow)
+            {
+                string foreColor = "\x1b[38;2" + ";" + red + ";" + green + ";" + blue + "m";
+                Console.Write(foreColor + c);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            return ""; 
+        }
+        public static string WriteLineColor(string userTextToMakeRainbow, int red, int green, int blue)
+        {
+            foreach (char c in userTextToMakeRainbow)
+            {
+                string foreColor = "\x1b[38;2" + ";" + red + ";" + green + ";" + blue + "m";
+                Console.Write(foreColor + c);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            Console.WriteLine("");
+            return ""; 
         }
     }
 }
