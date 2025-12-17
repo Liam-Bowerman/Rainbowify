e5fe5e091096b5ac31a53af60e9dea2cd343f901
Liam-Bowerman <liboad0@student.oaisd.org>
2025-12-17 14:12:44 +0000

    Simplified Rainbowify.cs to reduce redendancy... AGAIN.

--- Rainbowify.cs
+++ Rainbowify.cs
@@ -43,41 +43,7 @@ namespace Rainbows
         }
         public static string WriteLine(string userTextToMakeRainbow)
         {
-            int i = 1;
-            foreach (char c in userTextToMakeRainbow)
-            {
-                string foreColor = "\x1b[38;2" + ";" + 255 + ";" + 0 + ";" + 0 + "m";
-                switch (i)
-                {
-                    case 1:
-                        foreColor = "\x1b[38;2" + ";" + 255 + ";" + 0 + ";" + 0 + "m";
-                        break;
-                    case 2:
-                        foreColor = "\x1b[38;2" + ";" + 242 + ";" + 136 + ";" + 29 + "m";
-                        break;
-                    case 3:
-                        foreColor = "\x1b[38;2" + ";" + 255 + ";" + 255 + ";" + 0 + "m";
-                        break;
-                    case 4:
-                        foreColor = "\x1b[38;2" + ";" + 0 + ";" + 255 + ";" + 0 + "m";
-                        break;
-                    case 5:
-                        foreColor = "\x1b[38;2" + ";" + 0 + ";" + 0 + ";" + 255 + "m";
-                        break;
-                    case 6:
-                        foreColor = "\x1b[38;2" + ";" + 128 + ";" + 0 + ";" + 128 + "m";
-                        break;
-                    default:
-                        break;
-                }
-                Console.Write(foreColor + c);
-                i++;
-                if (i > 6)
-                {
-                    i = 1;
-                }
-                Console.ForegroundColor = ConsoleColor.White;
-            }
+            Write(userTextToMakeRainbow);
             Console.WriteLine("");
             return "";
         }
@@ -93,12 +59,7 @@ namespace Rainbows
         }
         public static string WriteLineColor(string userTextToMakeRainbow, int red, int green, int blue)
         {
-            foreach (char c in userTextToMakeRainbow)
-            {
-                string foreColor = "\x1b[38;2" + ";" + red + ";" + green + ";" + blue + "m";
-                Console.Write(foreColor + c);
-                Console.ForegroundColor = ConsoleColor.White;
-            }
+            WriteColor(userTextToMakeRainbow, red, green, blue);
             Console.WriteLine("");
             return "";
         }
